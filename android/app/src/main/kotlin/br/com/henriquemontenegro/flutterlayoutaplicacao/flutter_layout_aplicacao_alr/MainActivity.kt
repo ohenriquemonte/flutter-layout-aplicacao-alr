c069e48507c45aaef02328161a126887eec7e991
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2020-12-30 16:12:15 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/henriquemontenegro/flutterlayoutaplicacao/flutter_layout_aplicacao_alr/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.henriquemontenegro.flutterlayoutaplicacao.flutter_layout_aplicacao_alr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
